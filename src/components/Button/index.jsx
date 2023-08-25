2449ac9844cc98893bb11847092c56572f872400
freddyvidigal <freddy181208@hotmail.com>
2023-08-25 13:23:48 -0300

    criando projeto inicial

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+import { Component } from "react";
+
+export class Button extends Component {
+  render() {
+    const { text, onclick, disabled } = this.props;
+    return (
+      <button className="button" onClick={onclick} disabled={disabled}>
+        {text}
+      </button>
+    );
+  }
+}
---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,13 @@
+import "../PostCard/styles.css";
+
+export const PostCard = ({ title, cover, body, id }) => (
+  <div className="post">
+    <img src={cover} alt={title} />
+    <div className="post-content">
+      <h2>
+        {title} {id}
+      </h2>
+      <p>{body}</p>
+    </div>
+  </div>
+);
